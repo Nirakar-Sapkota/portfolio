d1ad1f2bc7404a62ee1967e1cdfc42efbc1a0ee7
Nirakar Sapkota <139460959+Nirakar-Sapkota@users.noreply.github.com>
2024-02-15 14:57:29 +1100

    width-auto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import{styles} from '../styles'
 import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
-    <section className ="relative w-full h-screen mx-auto">
+    <section className ="relative w-auto h-screen mx-auto">
     <div className={`${styles.paddingX} absolute inset-10 top-[130px] 
       max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
       <div className='flex flex-col justify-center items-center mt-5'>
