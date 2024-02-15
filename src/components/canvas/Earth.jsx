7fa5c05a3f80fd241f05a03ad3a44d7b7fa750a7
Nirakar Sapkota <139460959+Nirakar-Sapkota@users.noreply.github.com>
2024-02-16 10:39:54 +1100

    Update Earth.jsx
    added earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import {Suspense} from 'react';
 import {Canvas} from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { OrbitControls,SpotLight, useGLTF } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import CanvasLoader from '../Loader.jsx';
 const Earth = () => {
+
   const earth = useGLTF( './planet/scene.gltf')
+  
   return (
     <primitive
+   
       object={earth.scene}
-      scale={2.5}
+      scale={0.00040}
       position-y={0}
       rotation-y={0}
-    />  )
+    /> 
+     )
+     
 }
 
 const EarthCanvas=()=>{
   return(<Canvas
+ 
       shadows
       frameloop='demand'
       camera={{ 
-        fov:45,
+        fov:25,
         near:0.1,
-        far:200,
+        far:1000,
         position:[-4,3,6]
     }}
+    
 > 
     <Suspense 
     
@@ -38,6 +45,20 @@ const EarthCanvas=()=>{
     <Earth/>
     
     </Suspense>
+    <mesh>
+ 
+    <hemisphereLight intensity={0.21}/>
+    <pointLight 
+    intensity={1.8}
+      position = {[30,10,-12]}
+    />
+      <SpotLight
+        position ={[-3,-8,8]}
+        penumbra={2}
+        intensity={3.5}
+        shadow-mapSize={1024}
+    />
+  </mesh>
     
     </Canvas>)
 }
